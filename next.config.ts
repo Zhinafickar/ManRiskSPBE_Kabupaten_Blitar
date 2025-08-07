cd56d2f860f630a74e00dcdfcaf14a5198d48cac
Zhinafickar <117022255+Zhinafickar@users.noreply.github.com>
2025-08-07 11:34:51 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,18 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'diskominfo.penajamkab.go.id',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'cdn-2.tstatic.net',
         port: '',
         pathname: '/**',
